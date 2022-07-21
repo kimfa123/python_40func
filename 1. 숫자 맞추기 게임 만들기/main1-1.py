4356b6105a5c220c95cb51196e386db0827eb01a
김지훈 <kimvz2088@gmail.com>
2022-07-21 20:00:04 +0900

    python_40-2functions

---
+++ 1. 숫자 맞추기 게임 만들기/main1-1.py
@@ -0,0 +1,24 @@
+import random
+
+random_number = random.randint(1, 100)
+
+print(random_number)
+
+
+# random.random()
+# 0.0에서부터 0.99999사이의 실수를 반환합니다.
+
+# random.uniform()
+# a와 b사이의 실수값을 반환합니다.
+
+# random.randint(a,b)
+# a와 b사이의 정수값을 반환합니다.
+
+# random.randrange(a,b)
+# a와 b사이의 정수값을 반환합니다.
+
+# random.randrange(a)
+# 인자가 하나일경우 0 부터 a사이의 정수값을 반환합니다.
+
+# random.choice(type)
+# type에는 문자열, 리스트, 튜플, range의 값을 입력받을 수 있고 무작위로 하나의 원소를 뽑는다.
